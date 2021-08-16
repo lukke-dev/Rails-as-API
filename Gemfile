1a820a16c88de17bf385dbc078a251b5d0371481
Lucas dos Santos <lucas.ffr2014@gmail.com>
2021-08-16 09:14:02 -0300

    using gem serializer and add /api/heroes

--- Gemfile
+++ Gemfile
@@ -23,3 +23,5 @@ end
 
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
+
+gem "active_model_serializers", "~> 0.10.12"
--- app/models/hero.rb
+++ app/models/hero.rb
@@ -1,3 +1,6 @@
 class Hero < ApplicationRecord
   validates :name, presence: true
+
+  scope :sorted_by_name, -> { order(:name) }
+  # scope :sorted_by_name, -> { order(name: :desc) }
 end
